1308a190628dc40b154e5c6d337bae646f866694
relibrana <relibrana@outlook.com>
2022-04-26 20:54:39 -0500

    SimpleProjectCreation

---
+++ src/test/java/com/api/postnet/PostNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.postnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
