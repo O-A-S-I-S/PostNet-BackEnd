17d26c2bb689ea2118c6fbdc538ea6d5df49ad2b
LuisArroyo1 <89022072+LuisArroyo1@users.noreply.github.com>
2022-05-29 18:24:36 -0500

    Add files via upload

---
+++ src/main/java/com/api/postnet/config/PaymentMethodServiceConfig.java
@@ -0,0 +1,11 @@
+package com.api.postnet.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentMethodServiceConfig {
+    @Bean
+    public ModelMapper modelMapperPaymentMethod(){return new ModelMapper();}
+}
