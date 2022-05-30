e951e4aa74e3429d4a12c4c9bb2ea609fa6b6523
LuisArroyo1 <u201716094@upc.edu.pe>
2022-05-29 20:54:01 -0500

    Agrega función jpql para filtrar citas por id
    Se crea la función para poder filtrar las citas por su id

--- src/main/java/com/api/postnet/repository/AppoimentRepository.java
+++ src/main/java/com/api/postnet/repository/AppoimentRepository.java
@@ -2,7 +2,11 @@ package com.api.postnet.repository;
 
 import com.api.postnet.entities.Appoiment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AppoimentRepository extends JpaRepository<Appoiment,Long> {
 
+    @Query("SELECT o From Appoiment o WHERE o.id=:appoimentId")
+    Appoiment findAppoimentByAppoimentId(@Param("appoimentId") Long appoimentId);
 }
