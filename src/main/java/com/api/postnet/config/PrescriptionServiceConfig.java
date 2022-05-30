9b6514908bbd921953c6a0e3dfccd0efac36ed18
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-05-30 12:44:53 -0500

    Instanciar el ModelMapper

--- src/main/java/com/api/postnet/config/PrescriptionServiceConfig.java
+++ src/main/java/com/api/postnet/config/PrescriptionServiceConfig.java
@@ -1,4 +1,13 @@
 package com.api.postnet.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PrescriptionServiceConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
