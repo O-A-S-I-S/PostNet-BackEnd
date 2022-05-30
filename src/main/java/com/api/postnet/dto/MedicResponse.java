b1782e6b4f594977d9f4eaa6582c4436111f08f2
LuisArroyo1 <u201716094@upc.edu.pe>
2022-05-29 20:30:12 -0500

    Creación MedicResponse
    Información relevante a mostrar sobre un médico.

---
+++ src/main/java/com/api/postnet/dto/MedicResponse.java
@@ -0,0 +1,22 @@
+package com.api.postnet.dto;
+
+public class MedicResponse {
+    private String surname;
+    private String lastName;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
