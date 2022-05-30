797281b5fd177bc7ab8f6a5f63e09377d4d0777b
LuisArroyo1 <u201716094@upc.edu.pe>
2022-05-29 20:33:53 -0500

    Creación appoimentRepository
    Se hace la creación del appoimentRepository, de momento estará vacío, luego se agregarán funciones tipo jpql

---
+++ src/main/java/com/api/postnet/repository/AppoimentRepository.java
@@ -0,0 +1,8 @@
+package com.api.postnet.repository;
+
+import com.api.postnet.entities.Appoiment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppoimentRepository extends JpaRepository<Appoiment,Long> {
+
+}
