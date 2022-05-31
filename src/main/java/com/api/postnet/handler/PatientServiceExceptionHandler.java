b64701586b92303abb427f37d56d88e51e11454e
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-05-30 20:59:54 -0500

    Se agregaron notaciones faltantes @ControllerAdvice @ResController para el correcto funcionamiento de la excepción de acceso a citas pro parte de un paciente.

--- src/main/java/com/api/postnet/handler/PatientServiceExceptionHandler.java
+++ src/main/java/com/api/postnet/handler/PatientServiceExceptionHandler.java
@@ -5,12 +5,16 @@ import com.api.postnet.exceptions.IncorrectAppointmentRequestException;
 import com.api.postnet.exceptions.PatientServiceExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
+@ControllerAdvice
+@RestController
 public class PatientServiceExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(Exception exception, WebRequest request){
